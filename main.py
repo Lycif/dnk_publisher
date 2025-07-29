edad67ae87e3fd66b8037b41ef81ba594de910fd
Lycif <zablotskiyalex001@gmail.com>
2025-07-29 19:55:39 +0300

    main.py

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 from telethon import TelegramClient, events
-from telethon.tl.types import MessageEntityCustomEmoji, MessageEntityTextUrl
+from telethon.sessions import StringSession
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-api_id = 28774428
-api_hash = 'a21dbda93eaa482f8fc5400eec247cbf'
-session_name = 'main'
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+session_string = os.environ["SESSION_STRING"]
 
-source_channel_id = 1002707233772
-TARGET_CHAT_ID = -1002725548194
+source_channel_id = 1002714330182
+target_group_id = -1002840952584
 
-EMOJI_ID = 5204394597651871345
-LINK = "https://t.me/dnk_news"
-TEXT = "ДНК * Україна 👈 Підписатися"
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-client = TelegramClient(session_name, api_id, api_hash)
-
-@client.on(events.NewMessage(chats=SOURCE_CHANNEL_ID))
+@client.on(events.NewMessage(chats=[source_channel_id]))
 async def handler(event):
-    try:
-        message = event.message
-        await client.send_message(TARGET_CHAT_ID, message, file=message.media)
-        await client.send_message(
-            TARGET_CHAT_ID,
-            TEXT,
-            entities=[
-                MessageEntityCustomEmoji(offset=4, length=1, document_id=EMOJI_ID),
-                MessageEntityTextUrl(offset=0, length=len(TEXT), url=LINK)
-            ]
-        )
-    except Exception as e:
-        print(f"❌ Ошибка: {e}")
+    text = event.raw_text
+    await client.send_message(target_group_id, text)
 
-client.start()
-print("✅ Бот запущен.")
-client.run_until_disconnected()
+with client:
+    client.run_until_disconnected()
