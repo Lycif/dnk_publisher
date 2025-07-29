178678137aa33254934f4b6737011acd0490f050
Lycif <zablotskiyalex001@gmail.com>
2025-07-29 21:38:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-raise Exception("Проверка обновления")
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+session_string = os.environ["SESSION_STRING"]
+
+source_channel_id = -1002707233772
+target_group_id = -1002725548194
+
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
+
+@client.on(events.NewMessage(chats=[source_channel_id]))
+async def handler(event):
+    text = event.raw_text
+    await client.send_message(target_group_id, text)
+
+with client:
+    client.run_until_disconnected()
